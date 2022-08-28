5a0583cd81f8fbecbd472ddfb2f13459f83529c9
Huy <drac0123451@gmail.com>
2022-08-28 15:50:51 -0700

    added code to generate readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/readme-template.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptProject = readMeData => {
+    console.log(`
+  =================
+  Create readme
+  =================
+  `);
+  
+     // if there's no 'projects' array property, create one
+     if (!readMeData.projects) {
+      readMeData.projects = [];
+    }
+  
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ])
+    .then(projectData => {
+      readMeData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(readMeData);
+      } else {
+        return readMeData;
+      }
+    });
+  };
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+promptProject()
+  .then(readMeData => {
+    const readMe = generatePage(readMeData);
 
-// TODO: Create a function to initialize app
-function init() {}
+    fs.writeFile('./readme.md', readMe, err => {
+      if (err) throw new Error(err);
 
-// Function call to initialize app
-init();
+      console.log('ReadMe created! Check out readme.md in this directory to see it!');
+    });
+  });
