4fbd9db12f8002263ae41bcb6119a43ac178bea2
Huy <drac0123451@gmail.com>
2022-09-01 17:49:06 -0700

    generate markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,73 +1,68 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require('./src/readme-template.js')
+const path = require('path');
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const promptProject = readMeData => {
-    console.log(`
-  =================
-  Create readme
-  =================
-  `);
-  
-     // if there's no 'projects' array property, create one
-     if (!readMeData.projects) {
-      readMeData.projects = [];
-    }
-  
-    return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your project?'
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project (Required)'
-      },
-      {
-        type: 'checkbox',
-        name: 'languages',
-        message: 'What did you build this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-      },
-      {
-        type: 'input',
-        name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
-      },
-      {
-        type: 'confirm',
-        name: 'feature',
-        message: 'Would you like to feature this project?',
-        default: false
-      },
-      {
-        type: 'confirm',
-        name: 'confirmAddProject',
-        message: 'Would you like to enter another project?',
-        default: false
-      }
-    ])
-    .then(projectData => {
-      readMeData.projects.push(projectData);
-      if (projectData.confirmAddProject) {
-        return promptProject(readMeData);
-      } else {
-        return readMeData;
-      }
-    });
-  };
+const questions = [
+  {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?"
+  }, {
+      type: "input",
+      name: "email",
+      message: "What is your email address?"
+  },
+  {
+      type: "input",
+      name: "title",
+      message: "What is your project's name?"
+  },
+  {
+      type: "input",
+      name: "description",
+      message: "Please write a short description of your project:"
+  },
+  {
+      type: "list",
+      name: "license",
+      message: "What kind of license should your project have?",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+      type: "input",
+      name: "installation",
+      message: "What command should be run to install dependencies?",
+      default: "npm i"
+  },
+  {
+      type: "input",
+      name: "test",
+      message: "What command should be run to run tests?",
+      default: "npm test"
+  },
+  {
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using the repo?",
+  },
+  {
+      type: "input",
+      name: "contributing",
+      message: "What does the user need to know about contributing to the repo?",
+  }
+];
 
-promptProject()
-  .then(readMeData => {
-    const readMe = generatePage(readMeData);
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-    fs.writeFile('./readme.md', readMe, err => {
-      if (err) throw new Error(err);
+function init() {
+  inquirer.prompt(questions)
+      .then((inquirerResponses) => {
+          console.log("Generating README...");
+          writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
+      })
+}
 
-      console.log('ReadMe created! Check out readme.md in this directory to see it!');
-    });
-  });
+init();
